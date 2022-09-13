d056782a90128946da9eaf686db5c9c621060826
Toan Nguyen <toan.nguyen@medicaldirector.com>
2022-09-13 10:21:12 +0700

    feat: add new endpoint

---
+++ cyber-coffee-machie/Models/BrewCoffeeResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace cyber_coffee_machie.Models
+{
+    public class BrewCoffeeResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public DateTimeOffset Prepared { get; set; }
+    }
+}
